c592a0efe4f693eaa9bf225c1bd8861bf596e88f
DevBO <42234678+BobongKu@users.noreply.github.com>
2024-11-02 17:51:15 +0900

    2st commit

--- src/app/post/[slug]/page.js
+++ src/app/post/[slug]/page.js
@@ -1,6 +1,5 @@
-import BlogDetails from "@/src/components/Post/PostDetails";
+import PostDetails from "@/src/components/Post/PostDetails";
 import RenderMdx from "@/src/components/Post/RenderMdx";
-import Tag from "@/src/components/Elements/Tag";
 import siteMetadata from "@/src/utils/siteMetaData";
 import { allBlogs } from "contentlayer/generated";
 import { slug } from "github-slugger";
@@ -95,13 +94,8 @@ export default function BlogPage({ params }) {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
        <article>
-      <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
+      <div className="mb-8 text-center relative w-full h-[30vh] bg-dark">
         <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Tag
-            name={blog.tags[0]}
-            link={`/posts/${slug(blog.tags[0])}`}
-            className="px-6 text-sm py-2"
-          />
           <h1
             className="inline-block mt-6 font-semibold capitalize text-light text-2xl md:text-3xl lg:text-5xl !leading-normal relative w-5/6"
           >
@@ -121,45 +115,8 @@ export default function BlogPage({ params }) {
           sizes="100vw"
         />
       </div>
-      <BlogDetails blog={blog} slug={params.slug} />
-
-      <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
-        <div className="col-span-12  lg:col-span-4">
-          <details
-            className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
-            open
-          >
-            <summary className="text-lg font-semibold capitalize cursor-pointer">
-              Table Of Content
-            </summary>
-            <ul className="mt-4 font-in text-base">
-              {blog.toc.map((heading) => {
-                return (
-                  <li key={`#${heading.slug}`} className="py-1">
-                    <a
-                      href={`#${heading.slug}`}
-                      data-level={heading.level}
-                      className="data-[level=two]:pl-0  data-[level=two]:pt-2
-                                       data-[level=two]:border-t border-solid border-dark/40
-                                       data-[level=three]:pl-4
-                                       sm:data-[level=three]:pl-6
-                                       flex items-center justify-start
-                                       "
-                    >
-                      {heading.level === "three" ? (
-                        <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
-                          &nbsp;
-                        </span>
-                      ) : null}
-
-                      <span className="hover:underline">{heading.text}</span>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </details>
-        </div>
+      <PostDetails blog={blog} slug={params.slug} />
+      <div className="gap-y-3 lg:gap-12 sxl:gap-16 mt-8 px-10 xl:px-80 md:px-28 lg:px-40 xs:px-5s">
         <RenderMdx blog={blog} />
       </div>
     </article>
